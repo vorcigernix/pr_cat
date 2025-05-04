400c0275b7b95098ce6063bdad152ae7a0629eff
Adam Sobotka <adam.sobotka@meiro.io>
2025-05-04 12:26:49 +0200

    rebrand

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -157,7 +157,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
             >
               <a href="#">
                 <IconInnerShadowTop className="!size-5" />
-                <span className="text-base font-semibold">DORA Metrics</span>
+                <span className="text-base font-semibold">prcat</span>
               </a>
             </SidebarMenuButton>
           </SidebarMenuItem>
